70013be3e356f6eea15e9c247fdd0b298eb33af2
unidentified143 <sakshamsankhla767@gmail.com>
2024-01-24 11:42:04 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import ScrollToTop from "./components/ScrollToTop"; 
-import AppBar from './layouts/AppBar';
-import Home from './Home';
-import Footer from './layouts/Footer';
-import Gallery from './Gallery';
-import LoadingScreen from './components/LoaderScreen';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import AppBar from "./layouts/AppBar";
+import Home from "./Home";
+import Footer from "./layouts/Footer";
+import Gallery from "./Gallery";
+import LoadingScreen from "./components/LoaderScreen";
 
 // Components
-const NotFound = () => <div><h2>404 Not Found</h2></div>;
-
+const NotFound = () => (
+  <div>
+    <h2>404 Not Found</h2>
+  </div>
+);
+// const s =
 const App = () => {
   return (
     <>
-    <LoadingScreen />
-    <Router>
-      <div>
-        
-        <ScrollToTop />
-        <AppBar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/gallery" component={Gallery} />
-          <Route component={NotFound} />
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+      <LoadingScreen />
+      <Router>
+        <div>
+          <ScrollToTop />
+          <AppBar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/gallery" component={Gallery} />
+            <Route component={NotFound} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
     </>
   );
-}
+};
 
 export default App;
