0fc8a2e6de5472f3e0d99490ba198b696d70b4e5
sameer <samprogrammer1>
2024-02-03 00:11:51 +0530

    add all

--- src/Accommodation.js
+++ src/Accommodation.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Accommodation = () => {
   return (
     <section id="accommodation" className='mt-5' >
-        <h1 className="titleHead small-title-head text-center my-5 pt-5">Accommodation</h1>
+        <h1 className="titleHead small-title-head text-center my-5 pt-lg-5">Accommodation</h1>
         <div className="container mt-5" id="about">
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
